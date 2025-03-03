b9a87db0df79b901ab8d271083e3480297c935d9
Guilherme <alvesx2007@gmail.com>
2025-03-03 17:47:21 -0300

    alterações aula 2

--- MauiM3Agenda1/Helpers/SQLiteDatabaseHelper.cs
+++ MauiM3Agenda1/Helpers/SQLiteDatabaseHelper.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiM3Agenda1.Models;
 using SQLite;
-using MauiM3Agenda1.Models;
 
 namespace MauiM3Agenda1.Helpers
 {
